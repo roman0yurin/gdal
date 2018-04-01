84e08984a196f22ea05908d2465c8a4f73fa8aed
roman <gooood85@yandex.ru>
2018-04-02 00:12:56 +0500

    Добавление GDAL в режиме модульной сборки

--- gdal/gcore/gdalhashsetbandblockcache.cpp
+++ gdal/gcore/gdalhashsetbandblockcache.cpp
@@ -35,7 +35,11 @@
 #include <set>
 #include <vector>
 
-#include "cpl_config.h"
+#ifndef CMAKE_CONFIG
+    #include "cpl_config.h"
+#else
+    #include "cmake_config.h"
+#endif
 #include "cpl_error.h"
 #include "cpl_multiproc.h"
 
